fd2d409a8145b13131bef4d5181b93b40f17fc7d
ilsp <galanisd@gmail.com>
2017-05-17 13:30:46 +0300

    added workflow example

---
+++ workflow-example.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+@Grab('eu.openminted.script:openminted-script-groovy:0.0.1-SNAPSHOT')
+@groovy.transform.BaseScript eu.openminted.script.groovy.ScriptBase scriptBase
+
+catalog "DKPro":"classpath:/CatalogBase/engines.json'
+catalog "DKPro":'classpath:/CatalogBase/formats.json'
+
+read 'String' language 'en' params([
+    documentText: 'This is a test.'])
+apply 'OpenNlpSegmenter'
+apply 'OpenPosTagger'
+write 'Conll2006'
