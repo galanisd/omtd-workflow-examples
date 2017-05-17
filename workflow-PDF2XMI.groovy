f6f18288f70ca20b565ca099dfec2ad8d5feb7bf
ilsp <galanisd@gmail.com>
2017-05-17 18:25:40 +0300

    Updated

--- workflow-PDF2XMI.groovy
+++ workflow-PDF2XMI.groovy
@@ -3,7 +3,10 @@
 @Grab('eu.openminted.script:openminted-script-groovy:0.0.1-SNAPSHOT')
 @groovy.transform.BaseScript eu.openminted.script.groovy.ScriptBase scriptBase
 
-read 'Pdf' from language 'en' from './testInput/*.pdf'
+catalog "DKPro":'classpath:/CatalogBase/engines.json'
+catalog "DKPro":'classpath:/CatalogBase/formats.json'
+
+read 'String' from language 'en' from './testInput/*.pdf'
 write 'Xmi' to './testOutput/'
 
 
--- workflow-example.groovy
+++ workflow-example.groovy
@@ -3,7 +3,7 @@
 @Grab('eu.openminted.script:openminted-script-groovy:0.0.1-SNAPSHOT')
 @groovy.transform.BaseScript eu.openminted.script.groovy.ScriptBase scriptBase
 
-catalog "DKPro":"classpath:/CatalogBase/engines.json'
+catalog "DKPro":'classpath:/CatalogBase/engines.json'
 catalog "DKPro":'classpath:/CatalogBase/formats.json'
 
 read 'String' language 'en' params([
